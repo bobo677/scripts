09c1b80c0d0f7a4e2b1a8dd60ed7072731eba981
bobo677 <83439764+bobo677@users.noreply.github.com>
2021-12-13 10:42:32 +0800

    Update eu.sh

--- eu.sh
+++ eu.sh
@@ -272,7 +272,7 @@ getData() {
             CERT_FILE="/etc/v2ray/${DOMAIN}.pem"
             KEY_FILE="/etc/v2ray/${DOMAIN}.key"
         else
-            resolve=2a02:180:6:1::4390
+            resolve=8.17.250.15
             res=`echo -n ${resolve} | grep ${IP}`
             if [[ -z "${res}" ]]; then
                 colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
